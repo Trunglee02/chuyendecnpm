0f53b89f64d7dce175c891e2037fd0c69182f691
Trunglee02 <levantrung452002@gmail.com>
2023-09-23 15:25:14 +0700

    push project buoi4

--- bai4/src/main.js
+++ bai4/src/main.js
@@ -2,15 +2,18 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createRouter} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Login from "./pages/Login.vue";
-import Register from ".pages/Register.vue";
+import Register from "./pages/Register.vue";
 
 const routes = [
     { path: "/login", component: Login },
     { path: "/register", component: Register },
 ];
 
-const router = createRouter({ history: VueRouter.createWebHashHistory(), routes,});
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
 
 createApp(App).use(router).mount('#app')
